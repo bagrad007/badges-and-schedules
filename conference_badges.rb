bdb50e60f6e74483ef6411801d8a849593094904
Luke Gill <bagrad007@gmail.com>
2021-01-13 13:36:43 -0500

    commit tree b9c97d3d7a7bc0ecbc79952b305766e08629d77c

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+require 'pry'
+def badge_maker (name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (attendees)
+  empty_array = []
+    attendees.each do |attendee|
+       badge = "Hello, my name is #{attendee}."
+       empty_array << badge
+    end
+    return empty_array
+end
+
+def assign_rooms (attendees)
+        attendees.map.with_index(1) do |attendee, index|
+            badge = "Hello, #{attendee}! You'll be assigned to room #{index}!"
+        end
+        
+end
+
+def printer(attendees)
+     batch_badge_creator(attendees).each {|attendee| puts attendee}
+     assign_rooms(attendees).each {|attendee| puts attendee}
+    # binding.pry
+end
